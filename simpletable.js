4751ec05e1b5de8a4c0d5713681b16731d8da9e1
Hrishikesh S <hrishikesh.snrao@kaybus.com>
2014-04-27 14:23:35 +0530

    Initial commit.

---
+++ simpletable.js
@@ -0,0 +1,49 @@
+(function(namespace) {
+  namespace.RowView = Ember.View.extend({
+  classNames: ['mainpage-table-row'],
+  defaultTemplate: Ember.Handlebars.compile('<tr>{{#each property in view.contentProxy}}<td>{{property}}</td>{{/each}}<tr>'),
+  contentProxy: function() {
+    var content = this.get('content'),
+      keys = Ember.keys(content),
+      array = [];
+
+      keys.forEach(function(key) {
+        array.push([content[key]]);
+      });
+      return array;
+    }.property('content')
+
+  });
+
+  namespace.SimpleTableView = Ember.View.extend({
+    tagName: 'table',
+    classNames: ['mainpage-table'],
+    templateName: 'simpletable'
+  });
+
+  namespace.TableView = Ember.CollectionView.extend({
+    tagName: 'tbody',
+    contentBinding: 'controller.content',
+    rowView: Ember.computed.alias('parentView.rowView'),
+    emptyView: Ember.View.extend({
+      template: Ember.Handlebars.compile("No data")
+    }),
+
+    createChildView: function(viewClass, attrs) {
+      if (this.get('rowView')) {
+        viewClass = this.get('rowView');
+      }
+      else {
+        viewClass = 'row';
+      }
+      return this._super(viewClass, attrs);
+    }
+  });
+
+  namespace.TableHeaderView = Ember.View.extend({
+    tagName: 'thead',
+    contentBinding: 'controller.headers',
+    classNames: ['mainpage-table-row'],
+    defaultTemplate: Ember.Handlebars.compile('{{#each header in view.content}}<th>{{header}}</th>{{/each}}')
+  });
+}(App));
